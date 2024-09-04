ef51babb05a02558d47257a04097a3ed62835c9d
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-04 20:12:38 +0000

    App component updated to handle errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
+// app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './service/data.service';
+import { Observable } from 'rxjs';
+import { Courses } from './interfaces/courses';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'RxJS-Error-Handling-and-Debugging';
+
+  courses$: Courses[] = [];
+  error: string | null = null;
+  loading: boolean = false;
+
+  constructor(public dataService: DataService) {}
+
+  fetchData() {
+    this.loading = true;
+    this.courses$ = [];
+    this.error = null;
+
+    this.dataService.simulateHttpRequest('assets/data.json').subscribe({
+      next: (data) => {
+        this.courses$ = data as Courses[];
+        this.loading = false;
+        console.log('Data fetched:', this.courses$);
+      },
+      error: (err) => {
+        console.log('An error occured', this.error);
+        this.error = 'Failed to fetch courses: ' + err.message;
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      },
+    });
+  }
 }
